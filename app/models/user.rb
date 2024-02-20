061fe534ab031970b57dfd7686a08bef54d7293c
Noel Bryant <noelsobryant@gmail.com>
2024-02-20 10:45:39 +0300

    implement user model and add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,19 @@
+# app/models/user.rb
+
 class User < ApplicationRecord
+  # Validations
+  validates :username, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 8 }
+
+  # Relationships
+  has_many :consultations, dependent: :destroy
+
+  # Secure password
+  has_secure_password
+
+  # Custom method to authenticate user by username
+  def self.authenticate(username, password)
+    user = find_by(username: username)
+    user if user && user.authenticate(password)
+  end
 end
