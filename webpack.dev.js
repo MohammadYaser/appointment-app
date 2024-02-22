ad0247ba606af3c6bcc70b8b87eee2120e7d87b2
MohammadYaser <yasersafi19@outlook.com>
2024-02-22 18:37:16 +0430

    fix linter errors

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,8 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
+const common = require('./webpack.common');
 
 module.exports = merge(common, {
-  mode: 'development',
-  devtool: 'inline-source-map',
+  mode: 'production',
+  devtool: 'source-map',
 });
